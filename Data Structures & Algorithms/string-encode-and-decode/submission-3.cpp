c77d0af8dbf00feb032d3b544f94f7176ace685d
NemithKalu <nemithk.23@cse.mrt.ac.lk>
2026-09-09 14:43:52 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+
+        string concat;
+
+        for (const string& str: strs) {
+            for (const char& character: str) {
+                concat += character;
+            }
+            // character in between input strings
+            concat += "/esc/";
+        }
+        return concat;
+    }
+
+    vector<string> decode(string s) {
+        
+        // buffer to insert characters in to
+        string buffer;
+        vector<string> decodedList;
+
+        for (int i = 0; i < s.size(); i++) {
+            char character = s[i];
+
+            // dump buffer into output list and skip escape characters
+            if (character == '/' && s.substr(i, 5) == "/esc/") {
+                decodedList.push_back(buffer);
+                i += 4;
+                buffer = "";
+            
+            // add characters to buffer if escape character not met
+            } else {
+                buffer += character;
+            }
+        }
+
+        return decodedList;
+
+    }
+};
